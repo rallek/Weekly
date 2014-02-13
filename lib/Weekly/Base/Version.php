92cf09e5b5eb2b39a1306aa723efd4b34f040e05
rallek <ralf@familie-koester.de>
2014-02-13 20:22:01 +0100

    bumping version

--- lib/Weekly/Base/Version.php
+++ lib/Weekly/Base/Version.php
@@ -25,7 +25,7 @@ class Weekly_Base_Version extends Zikula_AbstractVersion
     {
         $meta = array();
         // the current module version
-        $meta['version']              = '0.2.2';
+        $meta['version']              = '0.2.5';
         // the displayed name of the module
         $meta['displayname']          = $this->__('Weekly');
         // the module description
@@ -34,7 +34,7 @@ class Weekly_Base_Version extends Zikula_AbstractVersion
         $meta['url']                  = $this->__('weekly');
         // core requirement
         $meta['core_min']             = '1.3.5'; // requires minimum 1.3.5
-        $meta['core_max']             = '1.3.6'; // not ready for 1.3.7 yet
+        $meta['core_max']             = '1.3.99'; // not ready for 1.4.0 yet
 
         // define special capabilities of this module
         $meta['capabilities'] = array(
